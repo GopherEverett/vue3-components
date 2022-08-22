7efa4bc252a021bbd2a88102e9de3d50a0404faf
John Jacobs <john.a.jacobs1973@gmail.com>
2022-08-22 09:56:35 -0400

    vue3 upgrade

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h3>{{ msg }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    props: {
+        msg: String,
+    }
+}
+</script>
+
+
+<style>
+h3 {
+    color: #80cbc4;
+}
+</style>
